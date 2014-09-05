46995affe37b4b832f791e975ca646afb35c4ec9
Wilfried Klaebe <wklaebe@users.noreply.github.com>
2014-09-05 19:08:39 +0000

    fix announce.sh

--- announce.sh
+++ announce.sh
@@ -2,5 +2,5 @@
 
 DIR="$(dirname "$0")"
 
-"${DIR}"/announce.py -d "${DIR}"/announce.d/nodeinfo/ | gzip | alfred -s 158
-"${DIR}"/announce.py -d "${DIR}"/announce.d/statistics/ | gzip | alfred -s 159
+"${DIR}"/announce.py -d "${DIR}"/nodeinfo.d/ | gzip | alfred -s 158
+"${DIR}"/announce.py -d "${DIR}"/statistics.d/ | gzip | alfred -s 159
