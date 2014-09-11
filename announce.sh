edf1012a47e3598f9f2b6b6a622f65cdc6b0afee
Daniel Ehlers <ehlers@rz.uni-kiel.de>
2014-09-12 01:14:30 +0200

    Support for alfred's interface selector.
    The latest alfred version supports multiple alfred/mesh instances.
    Time to support it here, too.

--- announce.sh
+++ announce.sh
@@ -2,5 +2,18 @@
 
 DIR="$(dirname "$0")"
 
-"${DIR}"/announce.py -d "${DIR}"/nodeinfo.d/ | gzip | alfred -s 158
-"${DIR}"/announce.py -d "${DIR}"/statistics.d/ | gzip | alfred -s 159
+case $1 in
+  -i)
+    shift
+    ip link show dev $1 > /dev/null
+    test $? -ne 0 && exit
+    INTERFACE="-I $1"
+    ;;
+  -h|--help)
+    echo "Usage: $0 [-i <ifname>]"
+    exit
+    ;;
+esac
+
+"${DIR}"/announce.py -d "${DIR}"/nodeinfo.d/ | gzip | alfred $INTERFACE -s 158
+"${DIR}"/announce.py -d "${DIR}"/statistics.d/ | gzip | alfred $INTERFACE -s 159
