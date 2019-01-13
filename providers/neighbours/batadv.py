9724557229bdd2ef2739ebef1d603b42c8cb2452
Tobias Schramm <tobleminer@gmail.com>
2019-01-13 22:44:55 +0100

    Grab originator data from `batctl o`, not debugfs

--- providers/neighbours/batadv.py
+++ providers/neighbours/batadv.py
@@ -1,22 +1,25 @@
-import providers
 from glob import glob
+import providers
+from providers.util import call
+import re
+
+re_mac = re.compile('([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}')
 
 class Source(providers.DataSource):
     def required_args(self):
         return ['batadv_dev']
 
     def call(self, batadv_dev):
-        return (lambda neighbours: {
-            open("{}/address".format(path)).read().strip(): {"neighbours": {
-                neigh[0]: {"lastseen": neigh[1], "tq": neigh[2]}
-                for neigh in neighbours
-                if neigh[3] == path[len('/sys/class/net/{}/lower_'.format(batadv_dev)):]
-            }}
-            for path in
-                glob('/sys/class/net/{}/lower_*'.format(batadv_dev))
-        })([
-            (line[0], float(line[1].strip('s')), int(line[2].strip(')')), line[4].strip('[]:'))
-            for line in map(lambda l: l.replace('(', '').replace('[', '').split(),
-                open('/sys/kernel/debug/batman_adv/{}/originators'.format(batadv_dev)))
-            if line[0] == line[3]
-        ])
+        lines = call(['batctl', '-m', batadv_dev, 'o'])
+        neighbours = {}
+        prefix_lower = '/sys/class/net/{}/lower_'.format(batadv_dev)
+        for dev in glob(prefix_lower + '*'):
+            ifname = dev[len(prefix_lower):]
+            mac = open(dev + '/address').read().strip()
+            ifneighbours = {}
+            for line in lines[2:]:
+                fields = line.replace('(', '').replace(')', '').replace('[', '').replace(']:', '').split()
+                if fields[4] == ifname and fields[0] == fields[3]:
+                    ifneighbours[fields[0]] = {'lastseen': float(fields[1].strip('s')), 'tq': int(fields[2])}
+            neighbours[mac] = {'neighbours': ifneighbours}
+        return neighbours
