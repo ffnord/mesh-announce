2b47aa9f0e56f2d6629b63c7da492656a095da07
Tobias Schramm <tobleminer@gmail.com>
2019-12-09 13:31:05 +0100

    Implement batctl >= 2019.3 support
    Co-authored-by: herbetom <git@herbetom.de>

--- providers/util.py
+++ providers/util.py
@@ -1,7 +1,27 @@
 import subprocess
+import re
+
+RE_BATCTL_VERSION = re.compile('([0-9]+)')
+BATCTL_VERSION_2019_3 = [2019, 3]
 
 def call(cmdline):
     output = subprocess.check_output(cmdline)
     lines = output.splitlines()
     lines = [line.decode("utf-8") for line in lines]
     return lines
+
+def call_batctl(batadv_dev, args):
+    def get_batctl_version():
+        line = call(['batctl', '-v'])[0]
+        version_match = [ RE_BATCTL_VERSION.search(elem) for elem in line.split('.') ]
+        return [ int(match.groups()[0]) if match else None for match in version_match ]
+
+    batctl_version = get_batctl_version()
+
+    meshif = 'meshif'
+    if batctl_version < BATCTL_VERSION_2019_3:
+        meshif = '-m'
+
+    base_args = ['batctl', meshif, batadv_dev]
+
+    return call(base_args + args)
