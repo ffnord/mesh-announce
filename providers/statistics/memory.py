d8adbd59bb641191bef3cf77939f7f51f22b194d
aiyion.prime <git@aiyionpri.me>
2021-03-19 11:39:58 +0100

    Restructurize memory provider
    it is now ordered and pep8 conform

--- providers/statistics/memory.py
+++ providers/statistics/memory.py
@@ -1,9 +1,12 @@
 import providers
 
+
 class Source(providers.DataSource):
     def call(self):
         return dict(
             (key.replace('Mem', '').lower(), int(value.split(' ')[0]))
-            for key, value in map(lambda s: map(str.strip, s.split(': ', 1)), open('/proc/meminfo').readlines())
-            if key in ('MemTotal', 'MemFree', 'MemAvailable', 'Buffers', 'Cached')
+            for key, value in map(lambda s: map(str.strip, s.split(': ', 1)),
+                                  open('/proc/meminfo').readlines())
+            if key in ('Buffers', 'Cached', 'MemAvailable', 'MemFree',
+                       'MemTotal')
         )
