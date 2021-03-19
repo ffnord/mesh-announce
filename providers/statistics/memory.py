22cea990369a96390ea01868073dbcef01c12d4a
aiyion.prime <git@aiyionpri.me>
2021-03-19 11:39:16 +0100

    Add provider for memory.available
    closes #76

--- providers/statistics/memory.py
+++ providers/statistics/memory.py
@@ -5,5 +5,5 @@ class Source(providers.DataSource):
         return dict(
             (key.replace('Mem', '').lower(), int(value.split(' ')[0]))
             for key, value in map(lambda s: map(str.strip, s.split(': ', 1)), open('/proc/meminfo').readlines())
-            if key in ('MemTotal', 'MemFree', 'Buffers', 'Cached')
+            if key in ('MemTotal', 'MemFree', 'MemAvailable', 'Buffers', 'Cached')
         )
