45c25e058b6cc8a0893bef99e68665daaf2af827
aiyion.prime <git@aiyionpri.me>
2021-03-21 16:29:10 +0100

    Implement a basic WireGuard provider
    based on the fastd implementation

---
+++ providers/nodeinfo/software/wireguard/enabled.py
@@ -0,0 +1,11 @@
+import providers
+from os.path import exists
+
+
+class Source(providers.DataSource):
+    def call(self, vpn_protos):
+        if 'wireguard' in vpn_protos:
+            return exists("/sys/module/wireguard/initstate")
+
+    def required_args(self):
+        return ['vpn_protos']
